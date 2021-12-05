acf0fbf022a688dc6859e6ad4b4c7c8a45fc9299
Divyesh Vishwakarma <divyesh1099@gmail.com>
2021-12-05 16:13:39 +0530

    Final Commit

--- badboystyle/settings.py
+++ badboystyle/settings.py
@@ -167,5 +167,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '123ss@ajenw.com'
-EMAIL_HOST_PASSWORD = 'ijfioqjw'
+EMAIL_HOST_USER = 'badboystyle766@.com'
+EMAIL_HOST_PASSWORD = 'BADBOYSTYLE@0529'
