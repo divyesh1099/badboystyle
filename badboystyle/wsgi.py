5cfb5285c1a765d40f89ef80da27cfbd8af910e7
Divyesh Vishwakarma <divyesh1099@gmail.com>
2021-12-28 21:09:27 +0530

    wsgi.py file fixed

--- badboystyle/wsgi.py
+++ badboystyle/wsgi.py
@@ -7,13 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os, sys
+import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'badboystyle.settings')
-
-sys.path.append('/var/www')
-sys.path.append('/var/www/mysite')
 
 application = get_wsgi_application()
